ef3ec09e8c56740ea61f19a984aa55f524fb88a7
ipi <120220950+Diraa-moncef@users.noreply.github.com>
2023-12-10 23:47:14 -1000

    net_checking.py

--- net_checking.py
+++ net_checking.py
@@ -10,26 +10,27 @@ print("|#|        |##|##|##|   |###|      |#|        |###|/##/")
 print("|#|        |##|  |##|   |#|        |#|        |###|\##\ ")
 print("|#|        |##|  |##|   |#|        |#|        |###| \##\ ")
 print("|######|   |##|  |##|   |######|   |######|   |###|  \##\ ")
-print("1) trouver une adresse IP a partir du nom de domaine\n2) trouver un nom de domaine apartir d'une adresse IP\n3) trouver le nom du port a partir de son numero\n4)trouver le numero du port a partir de son nom\n\t!!!!!!!!!!vous devez entrez le numero du service qui vous convient!!!!!!!!!!")
+
+print("1) find an IP address from the domain name\n2) find a domain name from an IP address\n3) find the port name from its number\n4) find the port number from its name\n\t!!!!!!!!!!you must enter the number of the service that suits you!!!!!!!!!!")
 import socket
 while True:
-    num_serv=int(input("veuillez entrer le numero d'operation =>"))
+    num_serv=int(input("please enter the operation number=>"))
     if num_serv == 1:
-        ip = input("veuillez entrer le nom de domain =>")
+        ip =input(" please enter the domain name=>")
         ip_addr = socket.gethostbyname(str(ip))
         print(ip_addr)
     elif num_serv == 2:
-        domain = input("veuillez entrer votre adresse IPV4 =>")
+        domain = input(" please enter your IPV4 address =>")
         dn = socket.gethostbyaddr(str(domain))
         print(dn)
     elif num_serv == 3:
-        name_port=int(input("veuillez entrer le numero du port =>"))
+        name_port=int(input("please enter the port number =>"))
         port = socket.getservbyport(name_port)
         print(port)
     elif num_serv == 4:
-        num_port = input("veuillez entrer le nom du port =>")
+        num_port = input("please enter the port number =>")
         name = socket.getservbyname(num_port)
         print(name)
-        rep = input("si vous voulez repeter l'operation entrer (yes/non) :")
+        rep = input(" if you want to repeat the operation enter (yes/no)")
     else:
-        print("esseyer une autre fois ce parametre n'existe pas!!!")
+        print("try again this parameter does not exist!!!")
